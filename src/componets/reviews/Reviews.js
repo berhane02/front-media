f60dcd74fcf1ec0ff8f4fbaf7de247d4c50c3ae3
berhane02 <yberhane1@outlook.com>
2023-12-12 23:55:17 -0500

    fix bug

--- src/componets/reviews/Reviews.js
+++ src/componets/reviews/Reviews.js
@@ -1,13 +1,13 @@
 import { useEffect, useRef } from "react";
 import api from "../../api/axiosConfig";
-import { useParam } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import ReviewForm from "../reviewForm/ReviewForm";
 import React from "react";
 
 const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
   const revText = useRef();
-  let params = useParam();
+  let params = useParams();
   const movieId = params.movieId;
 
   useEffect(() => {
