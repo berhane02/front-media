42bc9a9842763582fbab1e7070bc5e5df53b44ae
berhane02 <yberhane1@outlook.com>
2023-12-22 23:51:02 -0500

    add folder for registeration

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: ' https://2b9a-2603-6010-e105-b070-5c5f-56d0-64c4-a396.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    //backend base url
+    baseURL: 'https://movie-backend-sprint.onrender.com/',
+    //headers: {"ngrok-skip-browser-warning": "true"}
 })
